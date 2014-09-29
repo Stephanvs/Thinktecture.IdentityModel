bdb761b54651a5431a50b0eb66eadfc7fa8ff10c
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2014-09-29 16:16:05 +0200

    Provided proof in form of unit test cases

---
+++ source/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Thinktecture.IdentityModel.Client;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Deserialize_Error_Response_With_UppercasePropertyNames()
+        {
+            var tokenResponse = new TokenResponse("{\"Error\":\"unauthorized_client\",\"ErrorDescription\":null}");
+
+            Assert.AreEqual(true, tokenResponse.IsError);
+        }
+
+        [TestMethod]
+        public void Deserialize_Error_Response_With_LowercasePropertyNames()
+        {
+            var tokenResponse = new TokenResponse("{\"error\":\"unauthorized_client\",\"ErrorDescription\":null}");
+
+            Assert.AreEqual(true, tokenResponse.IsError);
+        }
+    }
+}
